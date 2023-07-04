2b5c185dabdba24eeaaefa905012f5f61c61f5ed
sagarpatole113 <sagarpatole113@gmail.com>
2023-07-04 12:12:25 +0530

    Add project files.

---
+++ DotNetSecurtiy/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetSecurtiy.Model
+
+{
+    public class AuthDbContext: IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options):base(options)
+        {
+
+            
+        }
+    }
+}
---
+++ DotNetSecurtiy/ViewModel/Register.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetSecurtiy.ViewModel
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage = "Password and Confirm Password did not match")]
+        public string ConfirmPassword {get; set;}   
+
+    }
+}
